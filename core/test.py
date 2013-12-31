e538089148110e0b89001e8c40448ae4db7733e3
Sergey <sergey-isaev@live.ru>
2013-12-31 18:09:27 +0400

    Add speed-test

--- core/test.py
+++ core/test.py
@@ -1,8 +1,20 @@
 from core import colorz
 from os import path
-import time
+from time import time
+import sys
+import csv
 
-start = time.time()
-va = colorz("c:/Разобрать/anime-817437.jpeg", 6)
-print (list(va))
-print ('time: %f' % (time.time() - start))
+
+def testTime(size):
+    start = time()
+    colorz("c:/Разобрать/anime-817437.jpeg", 6, size)
+    finish = round(time() - start, 4)
+    return finish
+    
+writer = csv.writer(open("some.csv", "a", newline=''), dialect='excel')
+for i in range(200, 201):
+    sys.stdout.write('\rРазмер изображения: {0} на {0} px'.format(i))
+    testName = sorted(list(colorz("c:/Разобрать/anime-817437.jpeg", 6, (i,i))))
+    test = [testTime((i,i)) for x in range(10)]
+    sys.stdout.flush()
+    writer.writerow([i, testName, test])
---
+++ core/test2.py
@@ -0,0 +1,23 @@
+import csv
+from openpyxl import Workbook
+from openpyxl.cell import get_column_letter
+    
+reader = csv.reader(open("some.csv", "r", newline=''), dialect='excel')
+
+
+wb = Workbook()
+ws = wb.worksheets[0]
+ws.title = "Sheet 1"
+
+
+for row_index, row in enumerate(reader):
+    res = [float(s.strip("[]")) for s in row[2].split(',')]
+    res = round(sum(res) / len(res), 4)
+    for column_index, cell in enumerate(row):
+        column_letter = get_column_letter((column_index + 1))
+        if column_index == 2:
+            ws.cell('%s%s'%(column_letter, (row_index + 1))).value = res
+        else:
+            ws.cell('%s%s'%(column_letter, (row_index + 1))).value = cell
+
+wb.save("some.xls")
