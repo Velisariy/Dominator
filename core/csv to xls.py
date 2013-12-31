43625a2c6f29b90431226c986e308f37eab89264
Sergey <sergey-isaev@live.ru>
2013-12-31 18:53:07 +0400

    Change name

--- core/csv to xls.py
+++ core/csv to xls.py
@@ -9,13 +9,15 @@ wb = Workbook()
 ws = wb.worksheets[0]
 ws.title = "Sheet 1"
 
-
+    
 for row_index, row in enumerate(reader):
     res = [float(s.strip("[]")) for s in row[2].split(',')]
     res = round(sum(res) / len(res), 4)
     for column_index, cell in enumerate(row):
         column_letter = get_column_letter((column_index + 1))
-        if column_index == 2:
+        if column_index == 1:
+            pass
+        elif column_index == 2:
             ws.cell('%s%s'%(column_letter, (row_index + 1))).value = res
         else:
             ws.cell('%s%s'%(column_letter, (row_index + 1))).value = cell
