02d31534d9dd8095d62155ddaf219763d1d0d02f
Sergey Isaev <Sergey Isaev>
2016-06-04 13:55:54 +0400

    Сохранение в pdf

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"], "include-files" : "icons"}
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"], "include-files" : ["icons", "hello.html"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -10,8 +10,8 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(  name = "guifoo",
-        version = "0.1",
+setup(  name = "Dominator",
+        version = "0.2",
         description = "My GUI application!",
         options = {"build_exe": {"icon" : "icons/icon.ico"}},
         executables = [Executable("start.py", base=base)])
--- start.py
+++ start.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
 import sys
 from time import time
 from core import core
 from PyQt4 import QtCore, QtGui
 
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.colors import HexColor
 
 class MainWindow(QtGui.QMainWindow):
     '''
@@ -11,29 +17,35 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         self.resize(640, 480)
+        self.colors = []
+        self.filename = ''
         self.setWindowTitle("Доминатор")
         self.setWindowIcon(QtGui.QIcon('icons/icon.png'))
-        self.statusBar()
 
         self.openImage = QtGui.QAction(QtGui.QIcon('icons/fileopen.png'), 'Открыть файл', self)
         self.openImage.setShortcut('Ctrl+O')
         self.connect(self.openImage, QtCore.SIGNAL("triggered()"), self.showDialog)
+        
+        self.saveImage = QtGui.QAction(QtGui.QIcon('icons/save.png'), 'Сохранить', self)
+        self.saveImage.setShortcut('Ctrl+S')
+        self.saveImage.setDisabled(True)
+        self.connect(self.saveImage, QtCore.SIGNAL("triggered()"), self.saveDialog)
 
         self.refreshImageAction = QtGui.QAction(QtGui.QIcon('icons/refresh.png'), 'Обновить', self)
-        # Выключаем кнопку Обновить при запуске программы
         self.refreshImageAction.setDisabled(True)  
         self.connect(self.refreshImageAction, QtCore.SIGNAL('triggered()'), self.refreshImage)
 
         self.exit = QtGui.QAction(QtGui.QIcon('icons/exit.png'), 'Выход', self)
         self.connect(self.exit, QtCore.SIGNAL('triggered()'), QtCore.SLOT('close()'))
-
+        
         # Отвечает за счетчик количества цветов. По умолчанию 3.
         self.spinBox = QtGui.QSpinBox()
-        self.spinBox.setRange(1, 6)
+        self.spinBox.setRange(1, 15)
         self.spinBox.setValue(3)
 
         toolBar = self.addToolBar('Панель инструментов')
         toolBar.addAction(self.openImage)
+        toolBar.addAction(self.saveImage)
         toolBar.addSeparator()
         toolBar.addWidget(self.spinBox)
         toolBar.addAction(self.refreshImageAction)
@@ -53,34 +65,23 @@ class MainWindow(QtGui.QMainWindow):
     def procImage(self, filename):
         start = time()
         # Определяем список доминирующих цветов
-        colors = list(core.colorz(filename, self.spinBox.value()))
-        colors.sort()
+        self.colors = list(core.colorz(filename, self.spinBox.value()))
+        self.colors.sort()
         # Загружаем изображение и редактируем его
         pix = QtGui.QPixmap()
         pix.load(filename)
-        w = pix.width() * 1.0
-        h = pix.height() * 1.0
+        w = float(pix.width())
+        h = float(pix.height())
         k = round(w / h, 2)
 
         self.imgLabel.setPixmap(pix.scaled(600 * k, 600))
-
-        def matching(color):
-            '''
-            Сравнение цвета со средним значением
-            для читабельного отображения текста
-            '''
-            if int(color, 16) > int("888888", 16):
-                result = "black"
-            else:
-                result = "white"
-            return result
             
         self.hBoxColor = QtGui.QHBoxLayout()
         self.hBoxColor.setMargin(0)
         colorWidgets = []
         labelWidgets = []
         vBoxLabels = []
-        for key, color in enumerate(colors):
+        for key, color in enumerate(self.colors):
             colorWidgets.append(QtGui.QWidget())
             labelWidgets.append(QtGui.QLabel())
             vBoxLabels.append(QtGui.QVBoxLayout())
@@ -97,7 +98,7 @@ class MainWindow(QtGui.QMainWindow):
             labelWidgets[key].setText("#%s" % color)
             labelWidgets[key].setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
             labelWidgets[key].setTextInteractionFlags(QtCore.Qt.LinksAccessibleByMouse | QtCore.Qt.TextSelectableByMouse)
-            labelWidgets[key].setStyleSheet("QWidget { color: %s }" % matching(color))
+            labelWidgets[key].setStyleSheet("QWidget { color: %s }" % self.matching(color))
         # Добавляем полученный набор виджетов на экран
         paint = QtGui.QWidget()
         paint.setMaximumHeight(40)
@@ -113,19 +114,61 @@ class MainWindow(QtGui.QMainWindow):
 
         self.setCentralWidget(mainWidget)
         self.statusBar().showMessage("Выполнено за {} сек.".format(round(time() - start, 3)))
-
+        
+    def saveDialog(self):
+        file = QtGui.QFileDialog.getSaveFileName(self, caption="Сохранить изображение", filter="*.pdf")
+        if file:
+            self.savePdf(file)
+        
     def showDialog(self):
         self.filename = QtGui.QFileDialog.getOpenFileName(self, caption="Открыть изображение")
         if self.filename:
             self.refreshImageAction.setDisabled(False)
+            self.saveImage.setDisabled(False)
             self.procImage(self.filename)
+    
+    def savePdf(self, file):
+        colors = self.colors
+        imgfile = self.filename
+        canvas = Canvas(file, pagesize=A4)
+        pdfmetrics.registerFont(TTFont('Arial', 'font/Arial.ttf'))
+        
+        canvas.setFont('Arial', 16)
+        canvas.drawString(20, 800, "Доминирующие цвета")
+        
+        canvas.setFont('Arial', 12)
+        for key, color in enumerate(colors):
+            canvas.setFillColor(HexColor('#%s' % color))
+            canvas.rect(20,765 - 30 * key, 70, 25, fill=1, stroke=0)
+            
+            canvas.setFillColor(HexColor(self.matching(color)))
+            canvas.drawCentredString(50, 775 - 30 * key, '#{}'.format(color))
+            
+            canvas.drawImage(imgfile, 100, 390, 300, 400, preserveAspectRatio = True, anchor='nw')
+            
+        canvas.save()
+        
 
     def refreshImage(self):
         self.procImage(self.filename)
+        
+    @staticmethod
+    def matching(color):
+            '''
+            Сравнение цвета со средним значением
+            для читабельного отображения текста
+            '''
+            color.strip('#')
+            if int(color, 16) > int("888888", 16):
+                result = "#000000"
+            else:
+                result = "#ffffff"
+            return result
 
 def main():
     app = QtGui.QApplication(sys.argv)
     window = MainWindow()
+    window.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
     window.show()
     sys.exit(app.exec_())
     
